9f8b48161e437f0f8749d9e1bae1c4d52dd99557
poom9091 <poom90914@gmail.com>
2021-01-08 02:42:57 +0700

    add Intern CMTC

--- src/components/Act&Ex.vue
+++ src/components/Act&Ex.vue
@@ -2,8 +2,8 @@
   <div class=" mt-10 mb-16 flex  flex-col space-y-9 ">
       <h1 class="text-darkblue-cos text-4xl md:text-5xl font-black  text-center w-full">Activities & Experience</h1>
       <div class=" bg-red-800 px-6 md:px-14 max-h-max h-auto py-8 mx-5 lg:mx-24 space-y-9 rounded-3xl shadow-xl">
-        <NSC/>
-        <intren/>
+        <Board v-bind=NSC />
+        <Board v-bind=Intern />
       </div>
             
   </div>
@@ -11,13 +11,41 @@
 
 <script>
 
-import NSC from "./nsc.vue"
-import Intren from "./IntrenCMTC.vue"
+import Board from "./board.vue"
 export default {
+  el:'root',
+  data(){
+    return{
+      NSC:{
+        name : 'Thailand IT Contest Festival 2018 ',
+        link :'https://github.com/poom9091/NSC_medicine-cabinet-use-RFID-scanning',
+        texts : ['- Developed a medicine cabinet use RFID scanning system'],
+        list_imgs:[
+            {img:require('../assets/NSC/NSC_1.jpg')},
+            {img:require('../assets/NSC/NSC_2.jpg')},
+            {img:require('../assets/NSC/NSC_3.jpg')}
+        ],
+        name_buttom:'GitHub',
+        type:'1'
+      },
+      Intern:{
+        name : 'Intern at Chiangmai Technical College  in Information Technology',
+        link :'https://www.youtube.com/playlist?list=PLC4CZjlaBNKuTKzLGrugiwaIW3aOGnFwS',
+        texts : ['- Teacher Assistant','- Developed a weather detector to analyze weather for farmers to present to the Centerfor Information Technology and Vocational Workforce. In making videos on YouTube'],
+        list_imgs:[
+            {img:require('../assets/Intern/Intern_1.jpg')},
+            {img:require('../assets/Intern/Intern_2.jpg')},
+            {img:require('../assets/Intern/Intern_3.jpg')}
+        ],
+        name_buttom:'YouTube',
+        type:'2'
+      },
+  }
+  },
     components:{
-       NSC,
-       Intren
+       Board
     }
+
 }
 </script>
 
--- src/components/HomeButton.vue
+++ src/components/HomeButton.vue
@@ -1,6 +1,8 @@
 <template>
   <button @click="$router.push('/')" 
-  class="text-yellow-cos text-lg font-black border-yellow-cos border-2 rounded-lg p-0.5 px-2 hover:text-yellow-400 md:text-2xl md:border-4">Pubadee Klinocha</button>
+  class="text-yellow-cos text-lg font-black border-yellow-cos border-2 rounded-lg p-0.5 px-2 hover:text-yellow-400 md:text-2xl md:border-4 bg-darkblue-cos z-50">
+  Pubadee Klinocha
+  </button>
 </template>
 
 <script>
